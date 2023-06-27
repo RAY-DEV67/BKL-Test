27b4c56f131b50fee6bc96e87f2168838add9e4f
Elebor Henry <henryekene8@gmail.com>
2023-06-27 14:40:44 -0700

    Feat: Pricing and Plan section

---
+++ src/components/pricing.jsx
@@ -0,0 +1,152 @@
+import Checkmark from "./checkmark";
+
+function Pricing() {
+  return (
+    <div className="py-[40px] px-[20px] ">
+      <div className="flex flex-col items-center justify-center">
+      <h2 className="text-[24px] lg:text-[40px] font-[600] text-white text-center">
+        Pricing and Plans
+      </h2>
+      <p className="text-[14px] lg:text-[16px] lg:w-[400px] text-center font-[300] leading-[21px] text-[#ffffffcc]">
+        Lorem ipsum dolor sit amet consectetur. Aliquet scelerisque sem commodo
+        turpis ipsum.
+      </p>
+      </div>
+      <div className="lg:flex lg:flex-row lg:justify-center lg:gap-x-[20px]">
+      <div className="py-[40px] px-[24px] mt-[40px] rounded-[16px] gap-y-[24px] bg-[#A522c23d]">
+        <h3 className="text-white">Free</h3>
+        <p className="my-[20px] text-white text-[24px] font-[600]">$0</p>
+        <div className="flex flex-col items-center">
+          <button className="py-[16px] px-[40px] text-white bg-[#a522c3] rounded-[90px] w-[287px]">
+            Get Started
+          </button>
+        </div>
+        <div className="flex flex-row items-center w-[255px] lg:w-[100%] mt-[20px]">
+          <Checkmark/>
+          <p className="text-[14px] font-[300] text-white ml-[8px]">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </div>
+        <div className="flex flex-row items-center w-[255px] lg:w-[100%] mt-[20px]">
+          <Checkmark/>
+          <p className="text-[14px] font-[300] text-white ml-[8px]">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </div>
+        <div className="flex flex-row items-center w-[255px] lg:w-[100%] mt-[20px]">
+         <Checkmark/>
+          <p className="text-[14px] font-[300] text-white ml-[8px]">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </div>
+        <div className="flex flex-row items-center w-[255px] lg:w-[100%] mt-[20px]">
+          <Checkmark/>
+          <p className="text-[14px] font-[300] text-white ml-[8px]">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </div>
+      </div>
+
+      <div className="py-[40px] px-[24px] mt-[40px] rounded-[16px] gap-y-[24px] bg-[#2229d024]">
+        <h3 className="text-white">Lite</h3>
+        <p className="my-[20px] text-white text-[24px] font-[600]">$24</p>
+        <div className="flex flex-col items-center">
+          <button className="py-[16px] px-[40px] text-white bg-[#2229d0] rounded-[90px] w-[287px]">
+            Get Started
+          </button>
+        </div>
+        <div className="flex flex-row items-center w-[255px] lg:w-[100%] mt-[20px]">
+          <Checkmark/>
+          <p className="text-[14px] font-[300] text-white ml-[8px]">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </div>
+        <div className="flex flex-row items-center w-[255px] lg:w-[100%] mt-[20px]">
+          <Checkmark/>
+          <p className="text-[14px] font-[300] text-white ml-[8px]">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </div>
+        <div className="flex flex-row items-center w-[255px] lg:w-[100%] mt-[20px]">
+         <Checkmark/>
+          <p className="text-[14px] font-[300] text-white ml-[8px]">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </div>
+        <div className="flex flex-row items-center w-[255px] lg:w-[100%] mt-[20px]">
+          <Checkmark/>
+          <p className="text-[14px] font-[300] text-white ml-[8px]">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </div>
+        <div className="flex flex-row items-center w-[255px] lg:w-[100%] mt-[20px]">
+          <Checkmark/>
+          <p className="text-[14px] font-[300] text-white ml-[8px]">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </div>
+        <div className="flex flex-row items-center w-[255px] lg:w-[100%] mt-[20px]">
+          <Checkmark/>
+          <p className="text-[14px] font-[300] text-white ml-[8px]">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </div>
+      </div>
+
+      <div className="py-[40px] px-[24px] mt-[40px] rounded-[16px] gap-y-[24px] bg-[#0ea4c424]">
+        <h3 className="text-white">Standard</h3>
+        <p className="my-[20px] text-white text-[24px] font-[600]">$40</p>
+        <div className="flex flex-col items-center">
+          <button className="py-[16px] px-[40px] text-white bg-[#0ea4c4] rounded-[90px] w-[287px]">
+            Get Started
+          </button>
+        </div>
+        <div className="flex flex-row items-center w-[255px] lg:w-[100%] mt-[20px]">
+          <Checkmark/>
+          <p className="text-[14px] font-[300] text-white ml-[8px]">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </div>
+        <div className="flex flex-row items-center w-[255px] lg:w-[100%] mt-[20px]">
+          <Checkmark/>
+          <p className="text-[14px] font-[300] text-white ml-[8px]">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </div>
+        <div className="flex flex-row items-center w-[255px] lg:w-[100%] mt-[20px]">
+         <Checkmark/>
+          <p className="text-[14px] font-[300] text-white ml-[8px]">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </div>
+        <div className="flex flex-row items-center w-[255px] lg:w-[100%] mt-[20px]">
+          <Checkmark/>
+          <p className="text-[14px] font-[300] text-white ml-[8px]">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </div>
+        <div className="flex flex-row items-center w-[255px] lg:w-[100%] mt-[20px]">
+          <Checkmark/>
+          <p className="text-[14px] font-[300] text-white ml-[8px]">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </div>
+        <div className="flex flex-row items-center w-[255px] lg:w-[100%] mt-[20px]">
+          <Checkmark/>
+          <p className="text-[14px] font-[300] text-white ml-[8px]">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </div>
+        <div className="flex flex-row items-center w-[255px] lg:w-[100%] mt-[20px]">
+          <Checkmark/>
+          <p className="text-[14px] font-[300] text-white ml-[8px]">
+            Lorem ipsum dolor sit amet consectetur.
+          </p>
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default Pricing;
--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -2,6 +2,7 @@ import Features from "../components/features";
 import Hero from "../components/heroSection";
 import HowItWorks from "../components/howItWorks";
 import Navbar from "../components/navbar";
+import Pricing from "../components/pricing";
 import Testimonials from "../components/testimonials";
 
 function LandingPage() {
@@ -11,6 +12,7 @@ function LandingPage() {
         <Features/>
         <HowItWorks/>
         <Testimonials/>
+        <Pricing/>
     </div> );
 }
 
