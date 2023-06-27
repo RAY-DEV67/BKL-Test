2b3433ece1b3680847caa9455fac20ce077b2c63
Elebor Henry <henryekene8@gmail.com>
2023-06-26 21:32:01 -0700

    feat: Navbar for mobile and desktop view

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,7 +7,14 @@ function Navbar() {
         <img src={logo} alt="Logo" />
         <p className="text-white ml-[4px]">NEPTIS</p>
       </div>
-      <img src={hamburger} alt="hamburger" />
+      <img src={hamburger} alt="hamburger" className="lg:hidden" />
+      <ul className="hidden lg:flex w-[528px] justify-between items-center text-white">
+        <li>Home</li>
+        <li>Features</li>
+        <li>How it works</li>
+        <li>Pricing</li>
+        <button className="py-[20px] px-[40px] rounded-[90px] bg-[#fff] text-[#221c38]">Get Started</button>
+      </ul>
     </div>
   );
 }
